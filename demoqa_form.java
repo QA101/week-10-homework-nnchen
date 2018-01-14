97f3dd42f7a9a32fb4d5ae47efb2adc6e00d62ec
nnchen <32369145+nnchen@users.noreply.github.com>
2018-01-14 00:11:15 -0800

    Add files via upload

---
+++ demoqa_form.java
@@ -0,0 +1,38 @@
+package homework_week10;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demoqa_form {
+
+	WebDriver driver = null;
+	
+	public void fillDemoqaForm() throws Throwable {
+		
+		driver = new ChromeDriver();
+		
+		driver.navigate().to("http://demoqa.com/");
+		
+		driver.findElement(By.partialLinkText("http://demoqa.com/registration")).click();
+		
+		driver.findElement(By.name("first_name")).sendKeys("bob");
+		
+		driver.findElement(By.name("last_name")).sendKeys("vance");
+		
+		driver.findElement(By.name("radio_4[]")).click();
+		
+		driver.findElement(By.name("checkbox_5[]")).click();
+		
+		driver.findElement(By.name("dropdown_7")).click();
+		
+		driver.findElement(By.name("username")).sendKeys("testtest");
+		
+		driver.findElement(By.name("password")).sendKeys("testtest22");
+		
+		driver.findElement(By.name("pie_submit")).click();
+		
+		
+	}
+	
+}
