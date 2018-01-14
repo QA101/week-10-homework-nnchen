7daf2b60763d66b7309ceb02754ff736c34e2b67
nnchen <32369145+nnchen@users.noreply.github.com>
2018-01-14 00:22:40 -0800

    Add files via upload

---
+++ search_linkedin.java
@@ -0,0 +1,33 @@
+package homework_week10;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Assert;
+
+public class search_linkedin {
+
+	WebDriver driver = null;
+	
+	public void fillDemoqaForm() throws Throwable {
+		
+		driver = new ChromeDriver();
+		
+		driver.navigate().to("https://www.google.com");
+		
+		driver.findElement(By.name("q")).sendKeys("linkedin");
+		
+		driver.findElement(By.id("nqsbq")).sendKeys("Nicole Chen");
+		
+		driver.findElement(By.partialLinkText("https://www.linkedin.com/pub/dir/Nicole/Chen")).click();
+		
+		boolean expectedText;
+		Assert.assertTrue("Nicole Chen", expectedText);
+		
+		
+	}
+	
+}
+	
+
