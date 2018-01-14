35a55c5d70a978a51cfc4c33038941d539fd4220
nnchen <32369145+nnchen@users.noreply.github.com>
2018-01-14 00:31:42 -0800

    Add files via upload

---
+++ expedia_vacationpkg.java
@@ -0,0 +1,24 @@
+package homework_week10;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class expedia_vacationpkg {
+
+WebDriver driver = null;
+	
+	public void fillDemoqaForm() throws Throwable {
+		
+		driver = new ChromeDriver();
+		
+		driver.navigate().to("https://www.expedia.com/");
+		
+		driver.findElement(By.id("package-origin-hp-package")).sendKeys("Seattle, WA");
+		
+		driver.findElement(By.id("package-destination-hp-package")).sendKeys("Cancun, MX");
+		
+		driver.findElement(By.id("search-button-hp-package")).click();	
+
+	}
+}
